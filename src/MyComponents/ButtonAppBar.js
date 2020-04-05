10f659f03b666147d0fd509eff4261bcfde31908
Rudy <rudysilva22@gmail.com>
2020-04-05 12:44:44 +1000

    IT BR Logo Added

--- src/MyComponents/ButtonAppBar.js
+++ src/MyComponents/ButtonAppBar.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import logo from '../itbrlogo.png';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,9 +27,11 @@ export default function ButtonAppBar() {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar className={classes.toolbar}>
+        <Toolbar className={classes.toolbar} >
           <Typography variant="h6" className={classes.title}>
-            IT.BR Australia - Points Calculator
+              <img src={logo} alt="Logo" className="App-logo" />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              IT.BR Australia - Points Calculator.
           </Typography>
         </Toolbar>
       </AppBar>
